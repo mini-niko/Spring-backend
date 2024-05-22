31e57c82ea543704d3ff551b93565270dc1396f9
Maurício Xavier Oliveira <xavierdeoliveiramauricio@gmail.com>
2024-05-22 19:11:27 -0300

    First commit

---
+++ src/main/java/br/minikoapi/MinikoApiApplication.java
@@ -0,0 +1,13 @@
+package br.minikoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinikoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinikoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/minikoapi/MinikoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.minikoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinikoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
